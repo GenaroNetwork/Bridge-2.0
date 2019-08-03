e4231f24fabcaa51b6c371400dc6d8233c5175d9
Xudong Yang <fanderous@gmail.com>
2019-08-03 21:55:53 +0800

    测试 get_bucket_tx_info

---
+++ src/genaro.rs
@@ -0,0 +1,43 @@
+//use web3::futures::Future;
+use web3::Transport;
+use web3::types::{H256, BlockNumber, H160};
+use web3::helpers::{CallFuture, serialize};
+use serde::Deserialize;
+
+// `Genaro` namespace
+#[derive(Debug, Clone)]
+pub struct Genaro<T> {
+    transport: T,
+}
+
+impl<T: Transport> Genaro<T> {
+    pub fn new(transport: T) -> Self {
+        Genaro { transport }
+    }
+    // 获取交易信息: 购买空间
+    pub fn get_bucket_tx_info(&self, from_block: Option<BlockNumber>, to_block: Option<BlockNumber>) -> web3::helpers::CallFuture<Option<Vec<BucketTxInfo>>, T::Out> {
+        let from = serialize(&from_block.unwrap_or(BlockNumber::Earliest));
+        let to = serialize(&to_block.unwrap_or(BlockNumber::Latest));
+        CallFuture::new(self.transport.execute("eth_getBucketTxInfo", vec![from, to]))
+    }
+    // 获取交易信息: 购买流量
+    pub fn get_traffic_tx_info(&self, from_block: Option<BlockNumber>, to_block: Option<BlockNumber>) -> web3::helpers::CallFuture<H256, T::Out> {
+        let from = serialize(&from_block.unwrap_or(BlockNumber::Earliest));
+        let to = serialize(&to_block.unwrap_or(BlockNumber::Latest));
+        CallFuture::new(self.transport.execute("eth_getTrafficTxInfo", vec![from, to]))
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct BucketTxInfo {
+    address: H160,
+    #[serde(rename(deserialize = "bucketId"))]
+    bucket_id: String,
+    #[serde(rename(deserialize = "timeStart"))]
+    time_start: u32,
+    #[serde(rename(deserialize = "timeEnd"))]
+    time_end: u32,
+    backup: u32,
+    size: u32,
+    hash: H256,
+}
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use web3::futures::Future;
+use web3::types::{BlockNumber, Address};
+use std::convert::TryFrom;
+
+mod genaro;
+
+fn main() {
+    let (_eloop, ws) = web3::transports::WebSocket::new("ws://47.100.34.71:8547").unwrap();
+    let web3 = web3::Web3::new(ws.clone());
+    let blocks = web3.eth().block_number().wait().unwrap();
+    println!("{:#?}", blocks);
+
+    let accounts = web3.eth().accounts().wait().unwrap();
+    println!("{:#?}", accounts);
+
+    // 测试 get_bucket_tx_info
+    let g = genaro::Genaro::new(ws.clone());
+    let bucket_tx_info = g.get_bucket_tx_info(
+        Some(BlockNumber::Number(99999)),
+        Some(BlockNumber::Number(186399)))
+        .wait().unwrap();
+    println!("{:#?}", bucket_tx_info);
+
+    let addr_vec = hex::decode("871860e8854bc539ab2127b2c91637aebab22a1f").unwrap();
+    let mut addr = [0u8; 20];
+    addr.clone_from_slice(&addr_vec);
+    let balance = web3.eth().balance(addr.into(), None).wait().unwrap();
+    println!("{:#?}", balance)
+}
