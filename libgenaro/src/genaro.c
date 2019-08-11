b7c8e7c228c8326991d075e866a7f95281961b29
丁毅 <dingyi9982@qq.com>
2019-08-11 20:55:29 +0800

    add get buckets

--- libgenaro/src/genaro.c
+++ libgenaro/src/genaro.c
@@ -68,3 +68,83 @@ static void json_request_worker(uv_work_t *work)
 
     req->status_code = status_code;
 }
+
+static void get_buckets_request_worker(uv_work_t *work)
+{
+    get_buckets_request_t *req = work->data;
+    int status_code = 0;
+
+    req->error_code = fetch_json(req->http_options, req->encrypt_options,
+                                 req->options, req->method, req->path, NULL, req->body,
+                                 req->auth, &req->response, &status_code);
+
+    req->status_code = status_code;
+
+    int num_buckets = 0;
+    if (req->response != NULL &&
+        json_object_is_type(req->response, json_type_array)) {
+        num_buckets = json_object_array_length(req->response);
+    }
+
+    if (num_buckets > 0) {
+        req->buckets = calloc(num_buckets, sizeof(genaro_bucket_meta_t));
+        req->total_buckets = num_buckets;
+    }
+
+    struct json_object *bucket_item;
+    struct json_object *id;
+    struct json_object *name;
+    struct json_object *created;
+    struct json_object *bucketId;
+    struct json_object *type;
+    struct json_object *limitStorage;
+    struct json_object *usedStorage;
+    struct json_object *timeStart;
+    struct json_object *timeEnd;
+
+    for (int i = 0; i < num_buckets; i++) {
+        bucket_item = json_object_array_get_idx(req->response, i);
+
+        json_object_object_get_ex(bucket_item, "id", &id);
+        json_object_object_get_ex(bucket_item, "name", &name);
+        json_object_object_get_ex(bucket_item, "created", &created);
+        json_object_object_get_ex(bucket_item, "bucketId", &bucketId);
+        json_object_object_get_ex(bucket_item, "type", &type);
+        json_object_object_get_ex(bucket_item, "limitStorage", &limitStorage);
+        json_object_object_get_ex(bucket_item, "usedStorage", &usedStorage);
+        json_object_object_get_ex(bucket_item, "timeStart", &timeStart);
+        json_object_object_get_ex(bucket_item, "timeEnd", &timeEnd);
+
+        genaro_bucket_meta_t *bucket = &req->buckets[i];
+        bucket->id = json_object_get_string(id);
+        bucket->decrypted = false;
+        bucket->created = json_object_get_string(created);
+        bucket->bucketId = json_object_get_string(bucketId);
+        bucket->type = json_object_get_int(type);
+        bucket->name = NULL;
+        bucket->limitStorage = json_object_get_int64(limitStorage);
+        bucket->usedStorage = json_object_get_int64(usedStorage);
+        bucket->timeStart = json_object_get_int64(timeStart);
+        bucket->timeEnd = json_object_get_int64(timeEnd);
+
+        const char *encrypted_name = json_object_get_string(name);
+        if (!encrypted_name) {
+            continue;
+        }
+
+        char *decrypted_name = NULL;
+        int error_status = decrypt_meta_hmac_sha512(encrypted_name,
+                                                    req->encrypt_options->priv_key,
+                                                    req->encrypt_options->key_len,
+                                                    BUCKET_NAME_MAGIC,
+                                                    &decrypted_name);
+
+        if (!error_status) {
+            bucket->decrypted = true;
+            bucket->name = decrypted_name;
+        } else {
+            bucket->decrypted = false;
+            bucket->name = strdup(encrypted_name);
+        }
+    }
+}
